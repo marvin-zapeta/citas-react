094a6a9f7ddc56bea0468e011fafabddfe905eee
zapeta0 <marvinzapeta309@gmail.com>
2023-02-22 10:36:03 -0600

    citas react

---
+++ src/components/Formulario.jsx
@@ -0,0 +1,20 @@
+const Formulario = () => {
+  return (
+    <div className="w-1/2 lg:w-2/5">
+      <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+      <p className="text-lg mt-5 text-center">
+        Añade Pacientes y {''}
+        <span className="text-indigo-600 font-bold">Administrador</span>
+      </p>
+
+      <form>
+        
+      </form>
+    </div>
+
+    
+  )
+}
+
+export default Formulario
---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,10 @@
+const ListadoPacientes = () => {
+  return (
+    <div className="w-1/2 lg:w2/5">
+      <h2>Listado Pacientes</h2>
+    </div>
+    
+  )
+}
+
+export default ListadoPacientes
